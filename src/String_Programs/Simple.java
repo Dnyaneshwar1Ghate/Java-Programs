2ee5ec8c41846dcb19d77ed48d7a477d2ca3bb2e
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2025-02-14 06:40:05 +0530

    done

---
+++ src/String_Programs/Simple.java
@@ -0,0 +1,10 @@
+package String_Programs;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		String str="JAva";
+		System.out.println(str);
+	}
+}
