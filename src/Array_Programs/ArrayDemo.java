8e7cb3211a5c889b79eb64a1c550e57c78eb1e07
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2025-02-14 06:59:33 +0530

    new class added

---
+++ src/Array_Programs/ArrayDemo.java
@@ -0,0 +1,13 @@
+package Array_Programs;
+
+public class ArrayDemo {
+	public static void main(String[] args) {
+		
+		int []a= {1,5,6,3,2};
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(" "+a[i]);
+		}
+	}
+
+}
---
+++ src/Collection_Programs/ListCollectionDemo.java
@@ -0,0 +1,16 @@
+package Collection_Programs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListCollectionDemo {
+	
+	public static void main(String[] args) {
+		List<String> lst=new ArrayList<String>();
+		lst.add("Helo");
+		lst.add("java");
+		System.out.println(lst);
+		
+	}
+
+}
