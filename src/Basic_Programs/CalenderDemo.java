5a702aa2d3e74b496b0ea069dc048e641d2b88f4
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2025-02-14 06:55:05 +0530

    new class added

---
+++ src/Basic_Programs/CalenderDemo.java
@@ -0,0 +1,15 @@
+package Basic_Programs;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class CalenderDemo {
+
+	public static void main(String[] args) {
+/*	Calendar cal=new Calendar.getInstance();
+		SimpleDateFormat ad=new SimpleDateFormat("M/D/YYYY HH:MM:SS");
+		System.out.println(ad.format(cal.getTime()));
+		System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+		System.out.println(cal.get(Calendar.DAY_OF_WEEK));*/
+	}
+}
---
+++ src/Basic_Programs/ConstructorDemo.java
@@ -0,0 +1,18 @@
+package Basic_Programs;
+
+public class ConstructorDemo {
+	
+	public ConstructorDemo()
+	{
+		System.out.println("i am in the constructor");
+	}
+	public void getData()
+	{
+		System.out.println("i am in the method");
+	}
+public static void main(String[] args) {
+	
+	ConstructorDemo cd=new ConstructorDemo();
+	
+}
+}
